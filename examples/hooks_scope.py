9868e8df38f909a6778627c8a65818a3ccc651ee
Kuyugama <mail.kuyugama@gmail.com>
2025-10-14 16:21:31 +0300

    Update scope hook explanation in example

--- examples/hooks_scope.py
+++ examples/hooks_scope.py
@@ -2,7 +2,7 @@ from fundi import scan
 from fundi import inject, with_hooks
 
 
-# Graph hook will be called only at the addition of the dependency to the dependant's parameters
+# Scope hook will be called before the injection
 @with_hooks(scope=lambda scope, ci: scope.update(name="Kuyugama") if "name" not in scope else None)
 def dependency(name: str):
     print(f"dependency({name=!r})")
