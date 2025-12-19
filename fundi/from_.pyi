9ed2d4b4f7fe846f425add807028b55582f048c4
Kuyugama <mail.kuyugama@gmail.com>
2025-12-19 16:45:22 +0200

    Improve `from_(...)` overloads for generators
    If parameter `generator` was set to `False` ensure that type-checkers will infer Generator type, not the yielded value type

--- fundi/from_.pyi
+++ fundi/from_.pyi
@@ -4,6 +4,11 @@ from collections.abc import Generator, AsyncGenerator, Awaitable
 from contextlib import AbstractAsyncContextManager, AbstractContextManager
 
 T = typing.TypeVar("T", bound=type)
+# Send
+S = typing.TypeVar("S")
+# Yield
+Y = typing.TypeVar("Y")
+# Return
 R = typing.TypeVar("R")
 
 @overload
@@ -31,7 +36,7 @@ def from_(
     dependency: typing.Callable[..., Generator[R, None, None]],
     caching: bool = True,
     async_: bool | None = None,
-    generator: bool | None = None,
+    generator: typing.Literal[True] | None = None,
     context: bool | None = None,
     use_return_annotation: bool = True,
 ) -> R: ...
@@ -40,10 +45,28 @@ def from_(
     dependency: typing.Callable[..., AsyncGenerator[R, None]],
     caching: bool = True,
     async_: bool | None = None,
-    generator: bool | None = None,
+    generator: typing.Literal[True] | None = None,
     context: bool | None = None,
     use_return_annotation: bool = True,
 ) -> R: ...
+@overload
+def from_(
+    dependency: typing.Callable[..., Generator[Y, S, R]],
+    caching: bool = True,
+    async_: bool | None = None,
+    generator: typing.Literal[False] = False,
+    context: bool | None = None,
+    use_return_annotation: bool = True,
+) -> Generator[Y, S, R]: ...
+@overload
+def from_(
+    dependency: typing.Callable[..., AsyncGenerator[Y, S]],
+    caching: bool = True,
+    async_: bool | None = None,
+    generator: typing.Literal[False] = False,
+    context: bool | None = None,
+    use_return_annotation: bool = True,
+) -> AsyncGenerator[Y, S]: ...
 @overload
 def from_(
     dependency: typing.Callable[..., Awaitable[R]],
