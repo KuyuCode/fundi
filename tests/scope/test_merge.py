b07d3e0c913b90af0afb8f8e4acec148d3e956c3
Kuyugama <mail.kuyugama@gmail.com>
2026-01-10 10:12:54 +0200

    Add scope merge tests

---
+++ tests/scope/test_merge.py
@@ -0,0 +1,57 @@
+import typing
+from typing_extensions import NewType
+
+from fundi import scan
+from fundi.scope import Scope, TypeInstance, TypeFactory
+
+
+def test_replace():
+    class AClass:
+        pass
+
+    class BClass:
+        pass
+
+    def factory0():
+        pass
+
+    def factory1():
+        pass
+
+    initial: dict[str | type | NewType, typing.Any] = {
+        "key": "value",
+        AClass: TypeInstance(1),
+        BClass: TypeFactory(scan(factory0)),
+    }
+
+    scope = Scope(initial)
+    scope1 = Scope(
+        {"key": "another value", AClass: TypeInstance(2), BClass: TypeFactory(scan(factory1))}
+    )
+
+    scope_merged = scope | scope1
+
+    assert scope_merged.values == {"key": "another value"}
+    assert scope_merged.types == {AClass: 2}
+    assert scope_merged.factories == {BClass: scan(factory1)}
+
+
+def test_extend():
+    class AClass:
+        pass
+
+    class BClass:
+        pass
+
+    def factory0():
+        pass
+
+    scope = Scope({"base_key": "initial value"})
+
+    scope1 = Scope({"key": "value", AClass: TypeInstance(1), BClass: TypeFactory(scan(factory0))})
+
+    scope_merged = scope | scope1
+
+    assert scope_merged.values == {"key": "value", "base_key": "initial value"}
+    assert scope_merged.types == {AClass: 1}
+    assert scope_merged.factories == {BClass: scan(factory0)}
